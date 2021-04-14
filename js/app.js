856350d66b8d1afecc739e763bcad9c1795a7060
Cejay <cjvanwel@hotmail.nl>
2021-04-14 15:05:46 +0200

    Searchbutton style refined

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 const tags = document.querySelectorAll('.tag');
+const search_icon = document.getElementById('search-icon');
+const search_bar = document.getElementById('search-bar');
+
+setInterval("checkFocus()", 1);
 
 tags.forEach((tag) => {
     tag.addEventListener('click', () => {
         tag.classList.toggle('checked');
     })
 })
+
+function checkFocus() {
+    if (search_bar == document.activeElement) {
+        search_icon.style.transition = 0.1;
+        search_icon.style.opacity = 0;
+        search_icon.style.display= 'none';
+    } else {
+        search_icon.style.display = 'inline';
+        search_icon.style.transition = 0.1;
+        search_icon.style.opacity = 1;
+    }
+}
