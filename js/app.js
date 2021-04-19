b98dc87b45cf4d6d109b67df858f20ff400b5375
Cejay <cjvanwel@hotmail.nl>
2021-04-19 14:35:13 +0200

    dynamic content in javascript (load content in main tags)

--- js/app.js
+++ js/app.js
@@ -23,3 +23,90 @@ function checkFocus() { /** start checkFocus functie */
         }
     }
 }
+
+const homeBtn = document.getElementById('home');
+const coursesBtn = document.getElementById('courses');
+
+homeBtn.addEventListener('click', () => {
+    alert('home');
+    console.log(home);
+});
+coursesBtn.addEventListener('click', () => {
+    alert('courses');
+    console.log(courses);
+});
+
+
+const courses = fetch('pages/courses.html')
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        return response.text()
+    })
+    .then(function(html) {
+        // Initialize the DOM parser
+        var parser = new DOMParser();
+
+        // Parse the text
+        var doc = parser.parseFromString(html, "text/html");
+
+        // You can now even select part of that html as you would in the regular DOM 
+        // Example:
+        // var docArticle = doc.querySelector('article').innerHTML;
+
+        console.log(doc);
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
+
+    const home = fetch('pages/home.html')
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        return response.text()
+    })
+    .then(function(html) {
+        // Initialize the DOM parser
+        var parser = new DOMParser();
+
+        // Parse the text
+        var doc = parser.parseFromString(html, "text/html");
+
+        // You can now even select part of that html as you would in the regular DOM 
+        // Example:
+        // var docArticle = doc.querySelector('article').innerHTML;
+
+        console.log(doc);
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
+
+
+// let deferredPrompt;
+// const addBtn = document.querySelector('.add-button');
+// addBtn.style.display = 'none';
+
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+//     e.preventDefault();
+//     // Stash the event so it can be triggered later.
+//     deferredPrompt = e;
+//     // Update UI to notify the user they can add to home screen
+//     addBtn.style.display = 'block';
+  
+//     addBtn.addEventListener('click', (e) => {
+//       // hide our user interface that shows our A2HS button
+//       addBtn.style.display = 'none';
+//       // Show the prompt
+//       deferredPrompt.prompt();
+//       // Wait for the user to respond to the prompt
+//       deferredPrompt.userChoice.then((choiceResult) => {
+//           if (choiceResult.outcome === 'accepted') {
+//             console.log('User accepted the A2HS prompt');
+//           } else {
+//             console.log('User dismissed the A2HS prompt');
+//           }
+//           deferredPrompt = null;
+//         });
+//     });
+//   });
