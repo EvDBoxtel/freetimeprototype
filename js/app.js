c4b32d49097d8c3944a88fe1fec3c1373befd5a2
Cejay <cjvanwel@hotmail.nl>
2021-04-19 15:31:02 +0200

    Dynamic pages are added except profile page

--- js/app.js
+++ js/app.js
@@ -24,91 +24,24 @@ function checkFocus() { /** start checkFocus functie */
     }
 }
 
-const homeBtn = document.getElementById('home');
-const coursesBtn = document.getElementById('courses');
+const menuBtns = document.querySelectorAll('.menuBtn'); /** vraag elementen op met class 'menuBtn' */
 
-homeBtn.addEventListener('click', () => {
-    alert('home');
-    alert(home);
-    console.log(home);
-});
-coursesBtn.addEventListener('click', () => {
-    alert('courses');
-    alert(courses);
-    console.log(courses);
-});
+menuBtns.forEach((menuBtn) => { /** looped door alle elementen van class 'menuBtn' heen */
+    menuBtn.addEventListener('click', () => { /** voeg voor elke element een click functie toe */
+        let page_id = menuBtn.getAttribute("data-page-id"); /** vraag het data attribuut op bij de aangeklikte 'menuBtn' */
+        loadPage(page_id); /** roep de loadpage functie aan met de data van het data attribuut van de 'menuBtn' */
+    })
+})
 
-
-const courses = fetch('pages/courses.html')
+const loadPage = (pageId) => { 
+    fetch(pageId) /** start een asynchrone functie om data van de aangevraagde pagina ('pageId') op te halen  */
     .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
+        return response.text() /** geeft de data van de opgevraagde pagina weer als tekst */
     })
     .then(function(html) {
-        // Initialize the DOM parser
-        var parser = new DOMParser();
-
-        // Parse the text
-        var doc = parser.parseFromString(html, "text/html");
-
-        // You can now even select part of that html as you would in the regular DOM 
-        // Example:
-        // var docArticle = doc.querySelector('article').innerHTML;
-
-        console.log(doc);
+        document.querySelector('main').innerHTML = html; /** zet de opgevraagde data in een html element */
     })
     .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
+        console.log('Failed to fetch page: ', err);  /** geeft een error melding als het opvragen van de data niet goed verloopt */
     });
-
-    const home = fetch('pages/home.html')
-    .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
-    })
-    .then(function(html) {
-        // Initialize the DOM parser
-        var parser = new DOMParser();
-
-        // Parse the text
-        var doc = parser.parseFromString(html, "text/html");
-
-        // You can now even select part of that html as you would in the regular DOM 
-        // Example:
-        // var docArticle = doc.querySelector('article').innerHTML;
-
-        console.log(doc);
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
-
-
-// let deferredPrompt;
-// const addBtn = document.querySelector('.add-button');
-// addBtn.style.display = 'none';
-
-// window.addEventListener('beforeinstallprompt', (e) => {
-//     // Prevent Chrome 67 and earlier from automatically showing the prompt
-//     e.preventDefault();
-//     // Stash the event so it can be triggered later.
-//     deferredPrompt = e;
-//     // Update UI to notify the user they can add to home screen
-//     addBtn.style.display = 'block';
-  
-//     addBtn.addEventListener('click', (e) => {
-//       // hide our user interface that shows our A2HS button
-//       addBtn.style.display = 'none';
-//       // Show the prompt
-//       deferredPrompt.prompt();
-//       // Wait for the user to respond to the prompt
-//       deferredPrompt.userChoice.then((choiceResult) => {
-//           if (choiceResult.outcome === 'accepted') {
-//             console.log('User accepted the A2HS prompt');
-//           } else {
-//             console.log('User dismissed the A2HS prompt');
-//           }
-//           deferredPrompt = null;
-//         });
-//     });
-//   });
+};
