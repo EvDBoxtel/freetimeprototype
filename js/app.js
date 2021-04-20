cf935ddecf2219d36aa2a36d9144fbab7f18495b
Erik <erik.vandijk@student.fontys.nl>
2021-04-20 14:59:53 +0200

    Made a course for backpacking and a test file
    Scroll progress on the course page

--- js/app.js
+++ js/app.js
@@ -23,3 +23,13 @@ function checkFocus() { /** start checkFocus functie */
         }
     }
 }
+
+//backpackingcourse scroll progress
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
