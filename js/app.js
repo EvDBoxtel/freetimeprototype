fb32fcc6e23031b6da931418c40dcb6ab2a24e97
Cejay <cjvanwel@hotmail.nl>
2021-04-21 14:26:51 +0200

    js errors resolved

--- js/app.js
+++ js/app.js
@@ -8,7 +8,39 @@ if (document.title == 'Prototype FreeTime') {
         return response.text()
     }).then(function(html) {
         main.innerHTML = html;
-    })
+    });
+
+    let deferredPrompt;
+    const addBtn = document.querySelector('.add-button');
+    // addBtn.style.display = 'none';
+    
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI to notify the user they can add to home screen
+        addBtn.style.display = 'block';
+        addBtn.addEventListener('click', (e) => {
+            // hide our user interface that shows our A2HS button
+            addBtn.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+        });
+    });
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {  
+        addBtn.style.display = 'none';
+    }  
 }
 
 if ('serviceWorker' in navigator) {
@@ -73,43 +105,13 @@ const loadPage = (pageId) => {
 };
 
 //backpackingcourse scroll progress
-main.onscroll = function() {readProgression()};
+if (document.titel== 'Backpacking course') {
+    main.onscroll = function() {readProgression()};
 
-function readProgression() {
-  var winScroll = main.scrollTop;
-  var height = main.scrollHeight - main.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+    function readProgression() {
+    var winScroll = main.scrollTop;
+    var height = main.scrollHeight - main.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+    }    
 }
-
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-// addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
-    addBtn.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      addBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-    });
-});
-
-if (window.matchMedia('(display-mode: standalone)').matches) {  
-    addBtn.style.display = 'none';
-}  
